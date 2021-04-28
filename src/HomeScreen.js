10543faabe1829f79724c58f6af945636e54075b
Syed Baqir Raza <sbaqarskp786@gmail.com>
2021-04-28 21:27:18 +0500

    Netflix Clone

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./HomeScreen.css";
 import Nav from "./Nav";
-import Banner from './Banner'
+import Banner from "./Banner";
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
-<Banner/>
-
-      </div>
+      <Banner />
+    </div>
   );
 }
 
